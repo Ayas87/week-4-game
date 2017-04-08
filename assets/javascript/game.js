13769c7facd2ba2b2e64657594349b493817d496
Randy Lam <Randy Lam>
2017-04-08 00:11:13 -0700

    removed most of the html, decided to generate boxes via js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,23 +14,34 @@
 ////try using objects .. meaning plan so that it would be easy to incorporate other ideas and making the game modular.
 ////use sounds on attack... if time allows
 ////use sprites to indicate break points of aP if time allows
-var dbzConfig = {
-	charName : ['Goku', 'Vegeta', 'Trunks', 'Gohan'],
-	//anything that is modular/theme specific goes in here
-};
-var dbzRpgGame = new RpgGame(dbzConfig); //creates a dbz instanced object where charName is passed through
-dbzRpgGame.startGame(); //calls for game start.. test by consoling dbzRpgGame.charName. confirmed to work
-function RpgGame(config) {
-	var self = this; //used to make identifying scope easier
-	//consider figuring out a formula where each character gets a bonus hP and it can random damage calculations that will always yield to a win possibility... 
-	////this may not be possible. but it would have to follow the logic where 100hP+bonus hP slightly >= all cP * # of attacks required to win combined & aP is calculated as total enemy hP - (# attacks required * aP increment ... not sure if i want it additive or multiplicitive yet)
-	self.hP = 0; //health points.. minmum hP should be 100
-	self.baseHp = 100; //may not actually use this
-	self.aP = 0; //attack power.. needs to increment
-	self.cAp = 0; //counter attack power
-	self.charName = config.charName //this is where putting in the dbz config will pull the charName object that is passed through... you would apply .config to anything you want to vary per game
-	self.startGame = function () {
-		//functions required to load when the page initializes is passed through here so it can all be called at once
+$(document).ready(function() {
+	var dbzConfig = {
+		charName : ['Goku', 'Vegeta', 'Trunks', 'Gohan'],
+		//anything that is modular/theme specific goes in here
 	};
-	//create functions below
-};
+	var dbzRpgGame = new RpgGame(dbzConfig); //creates a dbz instanced object where charName is passed through
+	dbzRpgGame.startGame(); //calls for game start.. test by consoling dbzRpgGame.charName. confirmed to work
+	function RpgGame(config) {
+		var self = this; //used to make identifying scope easier
+		self.hP = 0; //health points.. minmum hP should be 100
+		self.baseHp = 100; //may not actually use this
+		self.aP = 0; //attack power.. needs to increment
+		self.cAp = 0; //counter attack power
+		self.charName = config.charName //this is where putting in the dbz config will pull the charName object that is passed through... you would apply .config to anything you want to vary per game
+		self.startGame = function () {
+			console.log(dbzRpgGame);
+			self.makeChars();
+			//functions required to load when the page initializes is passed through here so it can all be called at once
+		};
+		self.makeChars = function () {
+			for(var i=0; i < this.charName.length; i++) {
+				console.log('makeChar function is looping ' + [this.charName]);
+				var makeDiv = $('<div>');
+				makeDiv.addClass('class-box char-name').attr('data-char',this.charName[i]).text(this.charName[i]);
+				$('.your-char').append(makeDiv);
+			}
+			$('.your-char')
+		};
+		//create functions below
+	};
+});
